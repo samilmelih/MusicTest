2f35e543826569f1c9ba68364d28e5ceb74a7d75
Melih <sam96mel95@gmail.com>
2017-12-28 11:02:03 +0300

    Project Files

---
+++ MusicTest/Assets/Composer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Composer
+{
+	int melodyCount;
+	int currMelody;
+
+	List<int>[] graph;
+
+	public Composer(int melodyCount)
+	{
+		Random rnd = new Random();
+
+		this.melodyCount = melodyCount;
+		this.currMelody = rnd.Next(0, melodyCount);
+
+		graph = new List<int>[melodyCount];
+	}
+
+	public void AddMelody(string melodyStr)
+	{
+		int[] melody = ParseMelody(melodyStr);
+
+		graph[melody[0]] = new List<int>(melody.Length);
+		for(int i = 0; i < melody.Length; i++)
+		{
+			if(i != 0)
+				graph[melody[0]].Add(melody[i]);
+		}
+	}
+
+	public int NextMelody()
+	{
+		Random rnd = new Random();
+
+		int possibleMelodyCount = graph[currMelody].Count;
+		int nextMelodyIndex = rnd.Next(0, possibleMelodyCount);
+		currMelody = graph[currMelody][nextMelodyIndex];
+
+		return currMelody;
+	}
+
+	static int[] ParseMelody(string melody)
+	{
+		return Array.ConvertAll(melody.Split('-'), int.Parse);
+	}
+}
---
+++ MusicTest/Assets/MusicTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTest : MonoBehaviour
+{
+	public AudioSource audioSource;
+	AudioClip[] audioClips;
+	Composer composer;
+
+	float cooldownTime = 1f;
+	float cooldown = 1f;
+	bool isPlaying;
+
+	// Use this for initialization
+	void Start ()
+	{
+		InitializeAudioClips();
+	}
+
+	void InitializeAudioClips()
+	{
+		audioClips = Resources.LoadAll<AudioClip>("");
+		composer = new Composer(audioClips.Length);
+
+		foreach(AudioClip ac in audioClips)
+			composer.AddMelody(ac.name);
+	}
+
+	void Update()
+	{
+		if(isPlaying == true)
+		{
+			if(cooldown <= 0f)
+			{
+				cooldown = cooldownTime;
+				isPlaying = false;
+			}
+			else
+				cooldown -= Time.deltaTime;
+		}	
+	}
+
+	public void Button_Clicked()
+	{
+		if(isPlaying == false)
+		{
+			int nextMelody = composer.NextMelody();
+			audioSource.PlayOneShot(audioClips[nextMelody], 1f);
+
+			isPlaying = true;
+		}
+	}
+}
